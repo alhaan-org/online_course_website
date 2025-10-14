1dff18b2cdc35894cfd5f279a1b1b685554156c3
alhaan-org <alhaanahmed@nexiatech.org>
2025-10-14 23:19:32 +0500

    feat: Added Dropdown Script for toggle

---
+++ app.js
@@ -0,0 +1,14 @@
+window.addEventListener("DOMContentLoaded", function () {
+  // Todo Code here
+  const dropdownMenu = document.querySelector(".dropdown-menu");
+  const btn = document.querySelector(".menu-btn");
+  const btnIcon = this.document.querySelector("i");
+  btn.addEventListener("click", () => {
+    dropdownMenu.classList.toggle("toggled");
+    if (btnIcon.classList.contains("bi-x")) {
+      btnIcon.classList.replace("bi-x", "bi-list");
+    } else {
+      btnIcon.classList.replace("bi-list", "bi-x");
+    }
+  });
+});
